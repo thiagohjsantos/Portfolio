6d936ea18cb42e01f503dc1444b55040b47b0d5b
Thiago Santos <61206196+DevHenr@users.noreply.github.com>
2021-09-06 21:30:01 -0300

    Terceiro commit

--- js/script.js
+++ js/script.js
@@ -100,9 +100,8 @@ document.getElementById("Icon").addEventListener('click', function() {
     current_rotation += 360;
     
 	// rotate clockwise by 90 degrees
-    // document.getElementById("Icon").style.transform = 'rotate(' + current_rotation + 'deg)';
+    
     document.getElementById("Icon").style.transform = 'rotate(' + current_rotation + 'deg)';  
-    // document.getElementById("Icon").style.transform += 'rotateX(' + current_rotation + 'deg)'; 
    
 });
 
@@ -110,25 +109,8 @@ document.getElementById("Icon").addEventListener('click', function() {
 	$(".se-pre-con").fadeOut(12000);;
 	});
 
-
-
-// let imgSlide = doc.querySelectorAll(".imgSlide");
-
-// function showDivs(){
-//     for(let i = 0; i < imgSlide.length; i++) {
-//         imgSlide[i].classList.remove('show');
-//     }
-//     slideCount++;
-//     if(slideCount == imgSlide.length){slideCount = 0}
-//     imgSlide[slideCount].classList.add('show');
-//     setTimeout(showDivs, 5000);
-// }
-
-// setTimeout(showDivs, 2000);
-
-
 var counter = 1;
-  setInterval(function(){
+  var myTimer = setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
     counter++;
     if(counter > 6){
@@ -137,8 +119,261 @@ var counter = 1;
 }, 6000);
 
 ScrollOut({
-    targets: '.skills h1,.html,.slider,.contact form,.contact h1,.about-me-info, .about img'
+    targets: '.skills h1,.html,.slider,.contact form,.contact h1,.about-me-info,.about img'
 })
 
+
+var count = 1;
+const img1 = document.getElementById('stop')
+const img2 = document.getElementById('play')
+document.getElementById('playState').onclick = function() {
+   count++;
+   if(count == 2){
+    clearInterval(myTimer);
+    img1.style.display = 'none';
+    img2.style.display = 'unset';
+   }
+   if(count == 3){
+    // img.src = "img/stopButton.png";
+    img1.style.display = 'unset';
+    img2.style.display = 'none';
+    count = 1;
+    myTimer = setInterval(function(){
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if(counter > 6){
+          counter = 1;
+        }
+    }, 6000);
+  }
+};
+
+
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+class TextScramble {
+    constructor(el) {
+      this.el = el
+      this.chars = 'ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｸ'  // !<>-_\\/[]{}—=+*^?#________  ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｸ
+      this.update = this.update.bind(this)
+    }
+    setText(newText) {
+      const oldText = this.el.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+
+
+  class TextScramble2 {
+    constructor(el) {
+      this.el = el
+      this.chars = 'XXX'  // !<>-_\\/[]{}—=+*^?#________  ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｸ
+      this.update = this.update.bind(this)
+    }
+    setText(newText) {
+      const oldText = this.el.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud2">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+
+  class TextScramble3 {
+    constructor(el) {
+      this.el = el
+      this.chars = '01'  // !<>-_\\/[]{}—=+*^?#________  ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｸ
+      this.update = this.update.bind(this)
+    }
+    setText(newText) {
+      const oldText = this.el.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud2">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+  
+  // ——————————————————————————————————————————————————
+  // Example
+  // ——————————————————————————————————————————————————
+  
+  const phrases = [
+    'Olá,',
+    'Me chamo Thiago',
+    'Sou um...',
+    'Desenvolvedor Full Stack Jr.'
+  ]
+
+  const phrases2 = [
+    'Sobre mim_'
+  ]
+  
+  const phrases3 = [
+    'Projetos_'
+  ]
+
+  const el = document.querySelector('.text h1')
+  const fx = new TextScramble(el)
+  
+  let coun = 0
+  const next = () => {
+    fx.setText(phrases[coun]).then(() => {
+      setTimeout(next, 3000)
+    })
+    coun = (coun + 1) % phrases.length
+  }
+
+  next()
+
+
+  const el2 = document.querySelector('.about h1')
+  const fx2 = new TextScramble2(el2)
+
+  let coun2 = 0
+  const next2 = () => {
+    fx2.setText(phrases2[coun2]).then(() => {
+      setTimeout(next2, 3500)
+    })
+    coun2 = (coun2 + 1) % phrases2.length
+  }
+
+  next2()
+
+
+  const el3 = document.querySelector('.work h1')
+  const fx3 = new TextScramble3(el3)
+
+  let coun3 = 0
+  const next3 = () => {
+    fx3.setText(phrases3[coun3]).then(() => {
+      setTimeout(next3, 4000)
+    })
+    coun3 = (coun3 + 1) % phrases3.length
+  }
+
+  next3()
 
 })(window,document);
